96ab2231c48a871ff03578d939d7384a50866f3c
FedeGauna <fede.face.gauna@gmail.com>
2025-10-28 13:28:58 -0300

    Modificado data_prep.py. Stages en reproduccion

--- src/data_prep.py
+++ src/data_prep.py
@@ -10,7 +10,23 @@ def main():
 
     df = df.drop_duplicates()
     df = df.dropna()
+    df = df.drop(columns=['customer_id'])
 
+    for col in ['total_charges', 'monthly_charges', 'tenure_months', 'age']:
+        if col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+
+    col_numericas = df.select_dtypes(include=['number']).columns.tolist()
+    for col in col_numericas:
+        df[col] = df[col].fillna(df[col].median())
+
+    col_categoricas = df.select_dtypes(include=['object', 'category']).columns.tolist()
+    for col in col_categoricas:
+        df[col] = df[col].fillna('Unknown')
+
+    df['phone_service'] = df['phone_service'].map({'Yes':1,'No':0, 'No phone service':0})
+    
+    df = pd.get_dummies(df, columns=col_categoricas, drop_first=True)
 
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     df.to_csv(output_path, index=False)
